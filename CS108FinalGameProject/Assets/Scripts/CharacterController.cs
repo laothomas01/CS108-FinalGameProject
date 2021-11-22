5546df44973d0a0a957ba3e3bca08e951b5ab723
Thomas Lao <laothomas01@gmail.com>
2021-11-21 21:02:33 -0800

    .
    .

--- CS108FinalGameProject/Assets/Scripts/CharacterController.cs
+++ CS108FinalGameProject/Assets/Scripts/CharacterController.cs
@@ -8,12 +8,26 @@ public class CharacterController : MonoBehaviour
     //Physics Component
     Rigidbody2D rb2d;
     //our inputs for left and right value
-    float horizontalInput;
-    float moveSpeed;
-    float moveLeftRight;
+    private float horizontalInput;
+    [SerializeField]
+    private float moveSpeed;
+    private float movementInputDirection;
+    [SerializeField]
+    private float groundCheckRadius;
     Vector2 moveDirection;
     private Vector2 m_Velocity = Vector2.zero;
     [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f; // how much to smooth out the movement
+
+    //direction character is facing
+
+    private bool isFacingRight = true;
+    private bool isGrounded;
+
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
+
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -22,16 +36,51 @@ public class CharacterController : MonoBehaviour
     void Update()
     {
         getPlayerInput();
+        CheckMovementDirection();
+
+    }
+    void FixedUpdate()
+    {
+        Move(horizontalInput);
     }
     public void getPlayerInput()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        Move(horizontalInput);
+
     }
     private void Move(float horizontalInput)
     {
-        moveLeftRight = horizontalInput * moveSpeed;
-        Vector2 targetVelocity = new Vector2(moveLeftRight, rb2d.velocity.y);
+        movementInputDirection = horizontalInput * moveSpeed;
+        Vector2 targetVelocity = new Vector2(movementInputDirection, rb2d.velocity.y);
         rb2d.velocity = Vector2.SmoothDamp(rb2d.velocity, targetVelocity, ref m_Velocity, movementSmoothing);
+    }
+    private void CheckMovementDirection()
+    {
+        if (isFacingRight && movementInputDirection < 0)
+        {
+            Flip();
+        }
+        else if (!isFacingRight && movementInputDirection > 0)
+        {
+            Flip();
+        }
+    }
+    /*
+     * Will be used to check walls. Dont want to stick to walls. 
+     */
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(new Vector3(0, 180, 0));
+
+    }
+    private void onDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
